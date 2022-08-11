de846714e2a5aa474f16194a72af026c46fc81a7
Mai.nguyen <ngmai12@gmail.com>
2022-08-11 11:11:52 +0200

    update

--- views/player.py
+++ views/player.py
@@ -12,7 +12,6 @@ class PlayerViews:
 
         data = {
             "first_name":first_name, "last_name":last_name, "birthday":birthday, "sex":sex, "rank":rank
-
         }
         return data
 
@@ -21,30 +20,41 @@ class PlayerViews:
         pass
 
     def update_player(self):
-        #afficher liste des joueurs
-        pass
+
+        while True:
+            self.menu.display_update_player()
+            user_input=input()
+            
+            if user_input=="1":
+                print("Edit player")
+            
+            elif user_input=="2":
+                print("Create player")
+
+            elif user_input=="3":
+                print("return main menu")
+            
+            else:
+                self.menu.display_message("Invalid choice")
+       
     
-    def update_player(self):
-        #a refaire pas input
+    def menu_player(self):
+        
         while True:
             self.menu.display_player_menu()
             user_input=input()
 
             if user_input=="1":
-                #create player
-                print()
+                print("Create player")
             
             elif user_input=="2":
-                #update player
-                print()
+                print("update player")
 
             elif user_input=="3":
-               #display list by name
-                print()
+                print("player list sort by name")
 
             elif user_input=="4":
-                #display list by rank
-                print("")
+                print("player list sort by rank")
 
             elif user_input=="5":
                 print("return main menu")
